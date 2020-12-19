717abd06155ecf272f0027ac6127d9d2ad04b120
Earl Cameron <mr.e.cameron@gmail.com>
2020-12-18 21:54:47 -0500

    scripting outline

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * TODOLIST features:
+ * add todo
+ * * title
+ * * submission time
+ * * message/body
+ * store todo db?
+ * update todo
+ * delete todo
+ * set todo as completed
+ * filter todo
+ * color todo
+ * todo groups
+ */
+
+ 
